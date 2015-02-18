c0183a2d30241578861cbad3271790103d74fb15
xinlnit <452073512@qq.com>
2015-02-18 09:50:20 +0800

    最简单的web：Servlet Hello World

---
+++ src/main/java/com/xinInit/servlet/HelloWordAnnotationServlet.java
@@ -0,0 +1,37 @@
+/**
+ */
+package com.xinInit.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author xin
+ * @version 1.0(xin) 2015年2月18日 上午9:22:30
+ */
+@WebServlet(name="helloWordServlet",urlPatterns="/helloWorldServlet.do")
+public class HelloWordAnnotationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("utf-8");
+		response.getWriter().write("<h2>Servlet Hello World!</h2>"); 
+	}
+
+	
+
+}
